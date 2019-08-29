757b8f0beb1c9e60821cebf4642e59d58f070c99
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2019-08-29 15:39:28 -0300

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analizing extremelly simple NN and other models for mosquito classification',
+    author='Bruno Coelho',
+    license='',
+)
