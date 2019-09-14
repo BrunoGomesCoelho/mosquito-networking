3a29dc8357d8a24c5768647b6637cd602fb51daa
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2019-09-14 18:04:09 -0300

    "Remove dotenv dependency for colab"

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,7 +3,7 @@ import gc
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+#from dotenv import find_dotenv, load_dotenv
 
 import numpy as np
 import pandas as pd
@@ -101,6 +101,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    #load_dotenv(find_dotenv())
 
     main()
